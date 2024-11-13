da7025450f46d4547de165606e148183f9fe2c21
Brandon Ros <brandonros1@gmail.com>
2024-11-12 22:02:22 -0500

    gateway api

--- scripts/deploy-infrastructure.sh
+++ scripts/deploy-infrastructure.sh
@@ -8,12 +8,6 @@ kubectl apply -f ./deploy/k8s/charts/metrics-server.yaml
 kubectl wait --for=create --timeout=90s deployment/metrics-server -n kube-system
 kubectl rollout status deployment/metrics-server -n kube-system --timeout=90s --watch
 
-# traefik
-echo "deploying traefik"
-kubectl apply -f ./deploy/k8s/charts/traefik.yaml
-kubectl wait --for=create --timeout=90s deployment/traefik -n traefik
-kubectl rollout status deployment/traefik -n traefik --timeout=90s --watch
-
 # cert-manager
 echo "deploying cert-manager"
 kubectl apply -f ./deploy/k8s/charts/cert-manager.yaml
@@ -28,59 +22,60 @@ export BASE64_ENCODED_CERT_CONTENT=$(cat ~/.lima/debian-k3s/copied-from-guest/se
 export BASE64_ENCODED_KEY_CONTENT=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.key | base64)
 envsubst < ./deploy/k8s/certs/cluster-issuer.yaml | kubectl apply -f -
 
-# debian-k3s-tls
-echo "deploying debian-k3s-tls"
-kubectl apply -f ./deploy/k8s/certs/debian-k3s-tls.yaml
-kubectl wait --for=create --timeout=90s certificate/debian-k3s-tls -n traefik
+# traefik
+echo "deploying traefik"
+kubectl apply -f ./deploy/k8s/charts/traefik.yaml
+kubectl wait --for=create --timeout=90s deployment/traefik -n traefik
+kubectl rollout status deployment/traefik -n traefik --timeout=90s --watch
+
+# gateway-tls
+echo "deploying gateway-tls"
+kubectl apply -f ./deploy/k8s/certs/gateway-tls.yaml
+kubectl wait --for=create --timeout=90s certificate/gateway-tls -n traefik
+
+# gateway
+echo "deploying gateway"
+kubectl apply -f ./deploy/k8s/gateways/gateway.yaml
 
 # docker-registry
 echo "deploying docker-registry"
 kubectl apply -f ./deploy/k8s/charts/docker-registry.yaml
 kubectl wait --for=create --timeout=90s deployment/docker-registry -n docker-registry
 kubectl rollout status deployment/docker-registry -n docker-registry --timeout=90s --watch
-kubectl apply -f ./deploy/k8s/ingress/docker-registry-external-ingress.yaml
+kubectl apply -f ./deploy/k8s/routes/docker-registry-route.yaml
 
-# storage
-echo "deploying storage"
-export HOST_PATH="/mnt/chess_bot"
-envsubst < ./deploy/k8s/storage/local-path-pvc.yaml | kubectl apply -f -
+# # storage
+# echo "deploying storage"
+# export HOST_PATH="/mnt/chess_bot"
+# envsubst < ./deploy/k8s/storage/local-path-pvc.yaml | kubectl apply -f -
 
-# dns
-echo "reconfiguring coredns"
-export TRAEFIK_IP=$(kubectl -n traefik get svc traefik -o jsonpath='{.spec.clusterIP}')
-envsubst < deploy/k8s/dns/coredns-config.yaml | kubectl apply -f -
+# # dns
+# echo "reconfiguring coredns"
+# export TRAEFIK_IP=$(kubectl -n traefik get svc traefik -o jsonpath='{.spec.clusterIP}')
+# envsubst < deploy/k8s/dns/coredns-config.yaml | kubectl apply -f -
 
-# kube-prometheus-stack
-echo "deploying kube-prometheus-stack"
-kubectl apply -f ./deploy/k8s/charts/kube-prometheus-stack.yaml
-kubectl wait --for=create --timeout=90s deployment/kube-prometheus-stack-grafana -n monitoring
-kubectl rollout status deployment/kube-prometheus-stack-grafana -n monitoring --timeout=180s --watch
-kubectl apply -f ./deploy/k8s/ingress/grafana-external-ingress.yaml
+# # kube-prometheus-stack
+# echo "deploying kube-prometheus-stack"
+# kubectl apply -f ./deploy/k8s/charts/kube-prometheus-stack.yaml
+# kubectl wait --for=create --timeout=90s deployment/kube-prometheus-stack-grafana -n monitoring
+# kubectl rollout status deployment/kube-prometheus-stack-grafana -n monitoring --timeout=180s --watch
+# #kubectl apply -f ./deploy/k8s/ingress/grafana-external-ingress.yaml
 
-# loki-stack
-echo "deploying loki-stack"
-kubectl apply -f ./deploy/k8s/charts/loki-stack.yaml
-kubectl wait --for=create --timeout=90s statefulset/loki-stack -n monitoring
-kubectl rollout status statefulset/loki-stack -n monitoring --timeout=90s --watch
+# # loki-stack
+# echo "deploying loki-stack"
+# kubectl apply -f ./deploy/k8s/charts/loki-stack.yaml
+# kubectl wait --for=create --timeout=90s statefulset/loki-stack -n monitoring
+# kubectl rollout status statefulset/loki-stack -n monitoring --timeout=90s --watch
 
-# tempo
-echo "deploying tempo"
-kubectl apply -f ./deploy/k8s/charts/tempo.yaml
-kubectl wait --for=create --timeout=90s statefulset/tempo -n monitoring
-kubectl rollout status statefulset/tempo -n monitoring --timeout=90s --watch
-kubectl apply -f ./deploy/k8s/ingress/tempo-external-ingress.yaml
+# # tempo
+# echo "deploying tempo"
+# kubectl apply -f ./deploy/k8s/charts/tempo.yaml
+# kubectl wait --for=create --timeout=90s statefulset/tempo -n monitoring
+# kubectl rollout status statefulset/tempo -n monitoring --timeout=90s --watch
+# #kubectl apply -f ./deploy/k8s/ingress/tempo-external-ingress.yaml
 
-# ngrok
-echo "deploying ngrok"
-envsubst < ./deploy/k8s/charts/ngrok-operator.yaml | kubectl apply -f -
-kubectl wait --for=create --timeout=90s deployment/ngrok-operator-manager -n ngrok
-kubectl rollout status deployment/ngrok-operator-manager -n ngrok --timeout=90s --watch
-
-# linkerd
-echo "deploying linkerd"
-export CA_CERT_PEM=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.crt | sed 's/^/      /')
-export ISSUER_CERT_PEM=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.crt | sed 's/^/            /')
-export ISSUER_KEY_PEM=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.key | sed 's/^/            /')
-envsubst < deploy/k8s/charts/linkerd.yaml | kubectl apply -f -
-kubectl wait --for=create --timeout=90s deployment/linkerd-destination -n linkerd
-kubectl rollout status deployment/linkerd-destination -n linkerd --timeout=90s --watch
+# # ngrok
+# echo "deploying ngrok"
+# envsubst < ./deploy/k8s/charts/ngrok-operator.yaml | kubectl apply -f -
+# kubectl wait --for=create --timeout=90s deployment/ngrok-operator-manager -n ngrok
+# kubectl rollout status deployment/ngrok-operator-manager -n ngrok --timeout=90s --watch
