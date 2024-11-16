c3ab94241a70a883afc1afe268751fb59f6e72c6
Brandon Ros <brandonros1@gmail.com>
2024-11-15 22:00:40 -0500

    linkerd traefik crd hell

--- scripts/create-vm.sh
+++ scripts/create-vm.sh
@@ -4,7 +4,7 @@ set -e
 
 # provision VM
 echo "provisioning VM"
-limactl start --name debian-k3s ./deploy/vm/debian-k3s.yaml
+limactl start --tty=false --name debian-k3s ./deploy/vm/debian-k3s.yaml
 
 # trust k3s-generated CA
 echo "trusting k3s-generated CA"
--- scripts/deploy-infrastructure.sh
+++ scripts/deploy-infrastructure.sh
@@ -16,22 +16,36 @@ kubectl rollout status deployment/cert-manager -n cert-manager --timeout=90s --w
 kubectl wait --for=condition=available --timeout=90s deployment/cert-manager-webhook -n cert-manager
 kubectl rollout status deployment/cert-manager-webhook -n cert-manager --timeout=90s --watch
 
-# cert-manager cluster-issuer
-echo "creating cluster-issuer"
+# cert-manager gateway-tls
+echo "creating gateway-tls"
 export BASE64_ENCODED_CERT_CONTENT=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.crt | base64)
 export BASE64_ENCODED_KEY_CONTENT=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.key | base64)
-envsubst < ./deploy/k8s/certs/cluster-issuer.yaml | kubectl apply -f -
+envsubst < ./deploy/k8s/certs/gateway-tls.yaml | kubectl apply -f -
+
+# cert-manager linkerd
+echo "creating linkerd"
+kubectl apply -f ./deploy/k8s/certs/linkerd.yaml
+
+# trust-manager
+echo "deploying trust-manager"
+kubectl apply -f ./deploy/k8s/charts/trust-manager.yaml
+kubectl wait --for=create --timeout=90s deployment/trust-manager -n trust-manager
+kubectl rollout status deployment/trust-manager -n trust-manager --timeout=90s --watch
+
+# two versions of gateway-api because linkerd needs old v1beta and traefik needs new v1
+if kubectl get crd gatewayclasses.gateway.networking.k8s.io -o json | jq -e '.status.storedVersions | contains(["v1beta1"])' >/dev/null; then
+    echo "GatewayClass v1beta1 CRD is installed"
+else
+    kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v0.8.1/experimental-install.yaml
+    kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.1/experimental-install.yaml
+    sleep 10 # allow rollout
+fi
 
 # traefik
 echo "deploying traefik"
 kubectl apply -f ./deploy/k8s/charts/traefik.yaml
 kubectl wait --for=create --timeout=90s deployment/traefik -n traefik
 kubectl rollout status deployment/traefik -n traefik --timeout=90s --watch
-
-# gateway-tls
-echo "deploying gateway-tls"
-kubectl apply -f ./deploy/k8s/certs/gateway-tls.yaml
-kubectl wait --for=create --timeout=90s certificate/gateway-tls -n traefik
 
 # gateway
 echo "deploying gateway"
@@ -113,14 +127,24 @@ envsubst < ./deploy/k8s/charts/ngrok-operator.yaml | kubectl apply -f -
 kubectl wait --for=create --timeout=90s deployment/ngrok-operator-manager -n ngrok
 kubectl rollout status deployment/ngrok-operator-manager -n ngrok --timeout=90s --watch
 
-# linkerd
-echo "deploying linkerd"
+# linkerd-crds
+echo "deploying linkerd-crds"
+kubectl apply -f ./deploy/k8s/charts/linkerd-crds.yaml
+
+# linkerd-control-plane
+echo "deploying linkerd-control-plane"
 export CA_CERT_PEM=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.crt | sed 's/^/      /')
 export ISSUER_CERT_PEM=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.crt | sed 's/^/            /')
 export ISSUER_KEY_PEM=$(cat ~/.lima/debian-k3s/copied-from-guest/server-ca.key | sed 's/^/            /')
-envsubst < deploy/k8s/charts/linkerd.yaml | kubectl apply -f -
+envsubst < deploy/k8s/charts/linkerd-control-plane.yaml | kubectl apply -f -
 kubectl wait --for=create --timeout=90s deployment/linkerd-destination -n linkerd
-#kubectl rollout status deployment/linkerd-destination -n linkerd --timeout=90s --watch TODO: weird bug with linkerd-destination
+kubectl rollout status deployment/linkerd-destination -n linkerd --timeout=90s --watch
+
+# linkerd-viz
+echo "deploying linkerd-viz"
+kubectl apply -f ./deploy/k8s/charts/linkerd-viz.yaml
+kubectl wait --for=create --timeout=90s deployment/web -n linkerd
+kubectl rollout status deployment/web -n linkerd --timeout=90s --watch
 
 # linkerd-viz route
 echo "deploying linkerd-viz route"
