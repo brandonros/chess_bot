fce12f112f85291a3a9f179a03195b20f5148697
Brandon Ros <brandonros1@gmail.com>
2024-11-18 08:04:14 -0500

    cleanup router

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ mod chess;
 
 use std::sync::Arc;
 
+use http::Method;
 use http_server::{HttpServer, Router};
 use simple_error::SimpleResult;
 use smol::{Executor, MainExecutor};
 
 async fn async_main(executor: Arc<Executor<'static>>) -> SimpleResult<()> {
+    // otional .env support
+    let _ = dotenvy::from_filename(".env");
+
     // logging
     env_logger::init();
 
@@ -18,14 +22,21 @@ async fn async_main(executor: Arc<Executor<'static>>) -> SimpleResult<()> {
 
     // build router
     let mut router = Router::new(executor.clone());
-    router.add_route("GET", "/ping", Arc::new(move |executor, req| Box::pin(routes::get_ping(executor, req))));
-    router.add_route("POST", "/move/best", Arc::new(move |executor, req| Box::pin(routes::get_best_move(executor, req))));
+    router.add_routes(vec![
+        (Method::GET, "/ping", Arc::new(move |ex, req| Box::pin(routes::get_ping(ex, req)))),
+        (Method::POST, "/move/best", Arc::new(move |ex, req| Box::pin(routes::get_best_move(ex, req)))),
+    ]);
     let router = Arc::new(router);
+
+    // log
+    log::info!("bound to {host}:{port}");
 
     // run server
     HttpServer::run_server(executor, host, port, router).await
 }
 
 fn main() -> SimpleResult<()> {
-    Arc::<Executor>::with_main(|ex| smol::block_on(async_main(ex.clone())))
+    Arc::<Executor>::with_main(|ex| {
+        smol::block_on(async_main(ex.clone()))
+    })
 }
